3308a5034a58ca657b99f3445067d69c468177c3
YoldoshevM <yoldoshevm396@gmail.com>
2026-06-13 15:36:17 +0500

    DTO Modells created

---
+++ Exam/src/TeamWork/Dtos/FoodCreateDto.cs
@@ -0,0 +1,10 @@
+﻿namespace TeamWork.Dtos;
+
+public class FoodCreateDto
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+    public bool IsAvailable { get; set; }
+    public long CategoryId { get; set; }
+}
---
+++ Exam/src/TeamWork/Dtos/FoodGetDto.cs
@@ -0,0 +1,12 @@
+﻿namespace TeamWork.Dtos;
+
+public class FoodGetDto
+{
+    public long FoodId { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+    public bool IsAvailable { get; set; }
+    public long CategoryId { get; set; }
+    public string? CategoryName { get; set; }
+}
