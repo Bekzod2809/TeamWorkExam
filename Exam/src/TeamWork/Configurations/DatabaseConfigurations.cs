277a9b3a4383e596a0732ae8f72e1a330e9fb934
Bekzod <bekzodjonxasanov7@gmail.com>
2026-06-13 14:38:14 +0500

    migration

---
+++ Exam/src/TeamWork/Configurations/DatabaseConfigurations.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TeamWork.Data;
+
+namespace TeamWork.Configurations;
+
+public static class DatabaseConfigurations
+{
+    // 'this WebApplicationBuilder builder' qismi juda muhim
+    public static void ConfigureDB(this WebApplicationBuilder builder)
+    {
+        // appsettings.json dagi nomi bilan bir xil bo'lishi shart
+        var connectionString = builder.Configuration.GetConnectionString("DatabaseConnection");
+
+        builder.Services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlServer(connectionString));
+    }
+}
