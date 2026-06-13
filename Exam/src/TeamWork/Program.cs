da73ee299ecdc951f2f1b75f0195a1e27e10be0f
YoldoshevM <yoldoshevm396@gmail.com>
2026-06-13 16:49:23 +0500

    SerilogConfiguration finished

--- Exam/src/TeamWork/Program.cs
+++ Exam/src/TeamWork/Program.cs
@@ -1,4 +1,4 @@
-
+using Serilog;
 using TeamWork.Configurations;
 
 namespace TeamWork
@@ -9,15 +9,16 @@ namespace TeamWork
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+            // Serilog sozlash
+            builder.ConfigureSerilog();
 
+            // Add services to the container.
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             builder.ConfigureDB();
-
 
             var app = builder.Build();
 
@@ -28,10 +29,27 @@ namespace TeamWork
                 app.UseSwaggerUI();
             }
 
+            // Har bir requestni log qiladi
+            app.UseSerilogRequestLogging();
+
             app.UseHttpsRedirection();
 
-            app.UseAuthorization();
+            // Exceptionlarni log qiladi
+            app.UseExceptionHandler(errorApp =>
+            {
+                errorApp.Run(async context =>
+                {
+                    var error = context.Features.Get<Microsoft.AspNetCore.Diagnostics.IExceptionHandlerFeature>();
+                    if (error != null)
+                    {
+                        Log.Error(error.Error, "Unhandled exception occurred");
+                    }
+                    context.Response.StatusCode = 500;
+                    await context.Response.WriteAsync("Internal Server Error");
+                });
+            });
 
+            app.UseAuthorization();
 
             app.MapControllers();
 
