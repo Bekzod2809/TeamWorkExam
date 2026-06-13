2bc00e2afdfeb89b0a61b96c4b32524b3f79aebc
Bekzod <bekzodjonxasanov7@gmail.com>
2026-06-13 16:07:44 +0500

    migration added SQL connected

--- Exam/src/TeamWork/Data/AppDbContext.cs
+++ Exam/src/TeamWork/Data/AppDbContext.cs
@@ -1,23 +1,16 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using TeamWork.Entities;
 
 namespace TeamWork.Data;
 
 public class AppDbContext : DbContext
 {
-
-
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Food> Foods { get; set; }
 
     public AppDbContext(DbContextOptions<AppDbContext> options)
      : base(options)
     {
-    }
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        modelBuilder.ApplyConfigurationsFromAssembly(
-            typeof(AppDbContext).Assembly);
-
-        base.OnModelCreating(modelBuilder);
     }
 }
 // push 001
