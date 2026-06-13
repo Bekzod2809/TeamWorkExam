ddddb4266cd61a15c1b320821656ab9bf174092e
YoldoshevM <yoldoshevm396@gmail.com>
2026-06-13 15:25:08 +0500

    Entity Modells created

---
+++ Exam/src/TeamWork/Entities/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace TeamWork.Entities;
+
+public class Category
+{
+    public long CategoryId { get; set; }
+    public string Name { get; set; }
+    public ICollection<Food> Foods { get; set; }
+}
